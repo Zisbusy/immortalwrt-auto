cd8c1cd667ffe1112361e72b22d6173f4aed7c4b
Zisbusy <Zisbusy@outlook.com>
2024-07-12 21:42:15 +0800

    First

---
+++ 23.05.2/run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ ! -n "$1" ] ;then
+echo "编译机型参数未添加！"
+exit 0
+fi
+
+if [ "$1" != "x86" ]&&[ "$1" != "erx" ] ;then
+echo "编译机型参数错误！"
+exit 0
+fi
+
+# 开始计时
+startTime=$(date +%Y%m%d-%H:%M:%S)
+startTime_s=$(date +%s)
+
+# 执行
+echo "自动化编译 immortalwrt v23.05.2" | tee -a /home/immortalwrt-auto/log.txt
+echo "工作目录：/home" | tee -a /home/immortalwrt-auto/log.txt
+
+if [ "$1" == "x86" ] ;then
+echo "目标机型：x86" | tee -a /home/immortalwrt-auto/log.txt
+fi
+
+if [ "$1" == "erx" ] ;then
+echo "目标机型：Ubiquiti EdgeRouter X" | tee -a /home/immortalwrt-auto/log.txt
+fi
+
+echo "安装一些编译要用的包..." | tee -a /home/immortalwrt-auto/log.txt
+dnf -y install perl wget rsync bzip2 patch tar ncurses-devel
+
+echo "克隆 immortalwrt 源码..." | tee -a /home/immortalwrt-auto/log.txt
+cd /home
+git clone https://github.com/immortalwrt/immortalwrt.git
+
+echo "设置 immortalwrt 版本 v23.05.2" | tee -a /home/immortalwrt-auto/log.txt
+cd /home/immortalwrt
+git checkout v23.05.2
+
+echo "更新 immortalwrt 源..." | tee -a /home/immortalwrt-auto/log.txt
+./scripts/feeds update -a
+echo "安装下载好的包: (可选)..." | tee -a /home/immortalwrt-auto/log.txt
+./scripts/feeds install -a
+
+echo "安装一些第三方包和一些设置..." | tee -a /home/immortalwrt-auto/log.txt
+
+echo "安装主题 Argon..." | tee -a /home/immortalwrt-auto/log.txt
+cp -rf /home/immortalwrt-auto/23.05.2/package/luci-theme-argon/ /home/immortalwrt/package/
+
+echo "设置：时区 Asia/Shanghai、NTP 服务器、默认网关..." | tee -a /home/immortalwrt-auto/log.txt
+rm -rf /home/immortalwrt/package/base-files/files/bin/config_generate
+cp -rf /home/immortalwrt/23.05.2/config/config_generate /home/immortalwrt/package/base-files/files/bin/
+chmod 755 /home/immortalwrt/package/base-files/files/bin/config_generate
+
+
---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "测试网络代理..." | tee -a /home/immortalwrt-auto/log.txt
+if ping -c 1 192.168.10.10 &> /dev/null; then
+    echo "代理地址有效 - 设置代理"
+    echo "设置本地网络代理：192.168.10.10:7890"
+    export http_proxy=http://192.168.10.10:7890
+    export https_proxy=http://192.168.10.10:7890
+else
+    echo "代理地址无效 - 不设置代理"
+fi
+
+echo "安装 git 工具..."
+dnf -y install git
+
+echo "克隆 immortalwrt-auto 源码..."
+git clone git@github.com:Zisbusy/immortalwrt-auto.git /home/immortalwrt-auto
+
+echo "执行自动化脚本..."
+chmod 777 /home/immortalwrt-auto/23.05.2/run.sh
+sh /home/immortalwrt-auto/23.05.2/run.sh $1
