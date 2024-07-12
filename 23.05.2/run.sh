7f3ba6af0e0bda3d1be5c6cabfe2cbf90daff71b
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-07-12 22:20:20 +0800

    Update run.sh

--- 23.05.2/run.sh
+++ 23.05.2/run.sh
@@ -52,4 +52,47 @@ rm -rf /home/immortalwrt/package/base-files/files/bin/config_generate
 cp -rf /home/immortalwrt-auto/23.05.2/config/config_generate /home/immortalwrt/package/base-files/files/bin/
 chmod 755 /home/immortalwrt/package/base-files/files/bin/config_generate
 
+if [ "$1" == "x86" ] ;then
+echo "设置：WAN口绑定 eth0、LAN口绑定 eth1..." | tee -a /home/immortalwrt-auto/log.txt
+rm -rf /home/immortalwrt/package/base-files/files/etc/board.d/99-default_network
+cp -rf /home/immortalwrt-auto/23.05.2/config/99-default_network /home/immortalwrt/package/base-files/files/etc/board.d/
+fi
 
+echo "复制编译配置文件..." | tee -a /home/immortalwrt-auto/log.txt
+if [ "$1" == "x86" ] ;then
+cp -rf /home/immortalwrt-auto/23.05.2/config/x86/.config /home/immortalwrt/
+fi
+
+if [ "$1" == "erx" ] ;then
+cp -rf /home/immortalwrt-auto/23.05.2/config/erx/.config /home/immortalwrt/
+fi
+
+# 执行脚本
+echo "定位到 OpenWrt 工作目录：/home/immortalwrt" | tee -a /home/immortalwrt-auto/log.txt
+cd /home/immortalwrt
+
+echo "允许 Root 用户编译" | tee -a /home/immortalwrt-auto/log.txt
+export FORCE_UNSAFE_CONFIGURE=1
+
+echo "获取 CPU 核心数" | tee -a /home/immortalwrt-auto/log.txt
+num=`cat /proc/cpuinfo |grep processor  | wc -l` 
+echo "开始编译 - "$num"线程" | tee -a /home/immortalwrt-auto/log.txt
+# make -j$num V=s
+
+# 计算时间并输出
+endTime=$(date +%Y%m%d-%H:%M:%S)
+endTime_s=$(date +%s)
+sumTime=$((endTime_s - startTime_s))
+hours=$((sumTime / 3600))
+minutes=$((sumTime % 3600 / 60))
+seconds=$((sumTime % 60))
+echo "编译结束！" | tee -a /home/immortalwrt-auto/log.txt
+echo "开始时间：$startTime" | tee -a /home/immortalwrt-auto/log.txt
+echo "结束时间：$endTime" | tee -a /home/immortalwrt-auto/log.txt
+if [ $hours -gt 0 ]; then
+    echo "总共耗时：$hours 小时 $minutes 分钟 $seconds 秒" | tee -a /home/immortalwrt-auto/log.txt
+elif [ $minutes -gt 0 ]; then
+    echo "总共耗时：$minutes 分钟 $seconds 秒" | tee -a /home/immortalwrt-auto/log.txt
+else
+    echo "总共耗时：$seconds 秒" | tee -a /home/immortalwrt-auto/log.txt
+fi
