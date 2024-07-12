8f70ce503ea7cb4705e3a647790746dc47c9a54c
Zisbusy <49525016+Zisbusy@users.noreply.github.com>
2024-07-12 22:09:13 +0800

    Update run.sh

--- 23.05.2/run.sh
+++ 23.05.2/run.sh
@@ -39,7 +39,7 @@ git checkout v23.05.2
 
 echo "更新 immortalwrt 源..." | tee -a /home/immortalwrt-auto/log.txt
 ./scripts/feeds update -a
-echo "安装下载好的包: (可选)..." | tee -a /home/immortalwrt-auto/log.txt
+echo "安装下载好的包..." | tee -a /home/immortalwrt-auto/log.txt
 ./scripts/feeds install -a
 
 echo "安装一些第三方包和一些设置..." | tee -a /home/immortalwrt-auto/log.txt
@@ -49,7 +49,7 @@ cp -rf /home/immortalwrt-auto/23.05.2/package/luci-theme-argon/ /home/immortalwr
 
 echo "设置：时区 Asia/Shanghai、NTP 服务器、默认网关..." | tee -a /home/immortalwrt-auto/log.txt
 rm -rf /home/immortalwrt/package/base-files/files/bin/config_generate
-cp -rf /home/immortalwrt/23.05.2/config/config_generate /home/immortalwrt/package/base-files/files/bin/
+cp -rf /home/immortalwrt-auto/23.05.2/config/config_generate /home/immortalwrt/package/base-files/files/bin/
 chmod 755 /home/immortalwrt/package/base-files/files/bin/config_generate
 
 
